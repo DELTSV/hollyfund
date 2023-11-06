e231fc58f9ea58ff4783e40c6633f1a94918a12f
Loïc Vanden Bossche <loloic066@gmail.com>
2023-11-06 18:21:22 +0100

    Removed deploy script in favor of hardhat ignition

--- workspaces/smartcontract/hardhat.config.ts
+++ workspaces/smartcontract/hardhat.config.ts
@@ -2,8 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
 
+import  dotenv from "dotenv";
+
+dotenv.config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
 
 export default config;
