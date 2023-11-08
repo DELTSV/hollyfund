1c4fd252858499168aacee477788888f1740b909
Loïc Vanden Bossche <loloic066@gmail.com>
2023-11-08 17:24:39 +0100

    Updated contract & tests

--- workspaces/smartcontract/contracts/HollyFund.sol
+++ workspaces/smartcontract/contracts/HollyFund.sol
@@ -4,20 +4,28 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HollyFund is ERC20 {
-    event NewCampaign(string name, uint targetAmount);
+    event NewCampaign(string name, uint256 targetAmount);
 
-    event NewInvestment(string name, uint investAmount);
+    event NewInvestment(string name, uint256 investAmount);
 
     struct Campaign {
         string title;
-        uint totalAmount;
-        uint targetAmount;
+        uint256 totalAmount;
+        uint256 targetAmount;
+        address producer;
         bool exists;
+    }
+
+    struct CampaignDto {
+        string title;
+        uint256 totalAmount;
+        uint256 targetAmount;
+        address producer;
     }
 
     struct Investment {
         address investor;
-        uint amount;
+        uint256 amount;
     }
 
     modifier isCampaignExists(string calldata _name) {
@@ -39,19 +47,19 @@ contract HollyFund is ERC20 {
         _mint(msg.sender, initialSupply);
     }
 
-    mapping (string => address) public campaignToProducer;
     mapping (string => Campaign) public campaigns;
-    mapping (string => mapping (address => uint)) public campaignToInvestments;
+    mapping (string => mapping (address => uint256)) public campaignToInvestments;
+    string[] private _campaignNames;
 
     function _isProducer(string calldata _name) internal returns (bool) {
-        return campaignToProducer[_name] == msg.sender;
+        return campaigns[_name].producer == msg.sender;
     }
 
-    function createCampaign(string calldata _name, uint _targetAmount) public isCampaignAlreadyExists(_name) {
+    function createCampaign(string calldata _name, uint256 _targetAmount) public isCampaignAlreadyExists(_name) {
         require(_targetAmount != 0, "Target amount must be greater than 0");
 
-        campaigns[_name] = Campaign(_name, 0, _targetAmount, true);
-        campaignToProducer[_name] = msg.sender;
+        campaigns[_name] = Campaign(_name, 0, _targetAmount, msg.sender, true);
+        _campaignNames.push(_name);
         emit NewCampaign(_name, _targetAmount);
     }
 
@@ -63,21 +71,33 @@ contract HollyFund is ERC20 {
         campaign.totalAmount += msg.value;
         campaignToInvestments[_name][msg.sender] += msg.value;
 
+        _mint(msg.sender, msg.value);
+
         this.transferFrom(msg.sender, address(this), msg.value);
 
         emit NewInvestment(_name, msg.value);
     }
 
-    function getCampaign(string calldata _name) public isCampaignExists(_name) view returns (string memory, uint) {
-        Campaign storage campaign = campaigns[_name];
-        return (campaign.title, campaign.targetAmount);
+    function getAllCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory campaignsArray = new Campaign[](_campaignNames.length);
+
+        for (uint i = 0; i < _campaignNames.length; i++) {
+            campaignsArray[i] = campaigns[_campaignNames[i]];
+        }
+
+        return campaignsArray;
     }
 
-    function getInvestment(string calldata _name) public isCampaignExists(_name) view returns (uint) {
+    function getCampaign(string calldata _name) public isCampaignExists(_name) view returns (string memory, uint256, uint256) {
+        Campaign storage campaign = campaigns[_name];
+        return (campaign.title, campaign.targetAmount, campaign.totalAmount);
+    }
+
+    function getInvestment(string calldata _name) public isCampaignExists(_name) view returns (uint256) {
         return campaignToInvestments[_name][msg.sender];
     }
 
-    function getCompletion(string calldata _name) public isCampaignExists(_name) view returns (uint) {
+    function getCompletion(string calldata _name) public isCampaignExists(_name) view returns (uint256) {
         Campaign storage campaign = campaigns[_name];
         return campaign.totalAmount * 100 / campaign.targetAmount;
     }
@@ -85,9 +105,11 @@ contract HollyFund is ERC20 {
     function claim(string calldata _name) public isCampaignExists(_name) isCampaignCompleted(_name) {
         require(_isProducer(_name) == true, "Only producer can claim");
 
-        uint amount = campaignToInvestments[_name][msg.sender];
-        campaignToInvestments[_name][msg.sender] = 0;
+        uint256 amount = campaigns[_name].totalAmount;
 
         this.transfer(msg.sender, amount);
+
+        payable(msg.sender).send(amount);
+        _burn(msg.sender, amount);
     }
 }
--- workspaces/smartcontract/test/CampaignFactory.ts
+++ workspaces/smartcontract/test/CampaignFactory.ts
@@ -7,12 +7,14 @@ import { ethers } from "hardhat";
 describe("HollyFund", function () {
   async function deployOneYearLockFixture() {
     const title = "The Matrix";
-    const targetAmount = 120;
-    const investAmount = 10;
+    const targetAmount = ethers.parseEther("9000");
+    const investAmount = ethers.parseEther("10");
+
+    await ethers.provider.send("hardhat_reset", []);
 
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const HollyFund = await ethers.getContractFactory("HollyFund",  owner);
+    const HollyFund = await ethers.getContractFactory("HollyFund", owner);
 
     const contract = await HollyFund.deploy(300);
 
@@ -20,12 +22,12 @@ describe("HollyFund", function () {
 
     await contract.createCampaign(title, targetAmount);
 
-    return { contract, title, targetAmount, owner, otherAccount, investAmount };
+    return {contract, title, targetAmount, owner, otherAccount, investAmount};
   }
 
   describe('Create campaign', function () {
     it('should not create campaign', async () => {
-      const { contract } = await loadFixture(
+      const {contract} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -36,7 +38,7 @@ describe("HollyFund", function () {
     });
 
     it('should not create campaign if it already exists', async () => {
-      const { contract, title, targetAmount } = await loadFixture(
+      const {contract, title, targetAmount} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -58,7 +60,7 @@ describe("HollyFund", function () {
     });
 
     it("Should emit an event on create a campaign", async function () {
-      const { targetAmount, contract } = await loadFixture(
+      const {targetAmount, contract} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -70,7 +72,7 @@ describe("HollyFund", function () {
 
   describe("Transactions", function () {
     it("Should transfer tokens between accounts", async function () {
-      const { otherAccount, contract } = await loadFixture(
+      const {otherAccount, contract} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -80,7 +82,7 @@ describe("HollyFund", function () {
     });
 
     it("Should fail if sender doesn’t have enough tokens", async function () {
-      const { contract, owner, otherAccount } = await loadFixture(
+      const {contract, owner, otherAccount} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -96,7 +98,7 @@ describe("HollyFund", function () {
     });
 
     it("Should update balances after transfers", async function () {
-      const { otherAccount, contract, owner } = await loadFixture(
+      const {otherAccount, contract, owner} = await loadFixture(
         deployOneYearLockFixture
       );
 
@@ -119,10 +121,9 @@ describe("HollyFund", function () {
         deployOneYearLockFixture
       );
 
-      await contract.transfer(otherAccount.address, 50);
       await contract.connect(otherAccount).approve(contract.getAddress(), investAmount);
 
-      await expect(contract.connect(otherAccount).invest(title, { value: investAmount })).not.to.be.reverted;
+      await expect(contract.connect(otherAccount).invest(title, {value: investAmount})).not.to.be.reverted;
     });
 
     it("Should pay invest amount", async function () {
@@ -130,12 +131,59 @@ describe("HollyFund", function () {
         deployOneYearLockFixture
       );
 
-      await contract.transfer(otherAccount.address, 50);
       await contract.connect(otherAccount).approve(contract.getAddress(), investAmount);
 
-      await expect(contract.connect(otherAccount).invest(title, { value: investAmount }))
+      await expect(contract.connect(otherAccount).invest(title, {value: investAmount}))
         .to.emit(contract, "NewInvestment")
         .withArgs(title, investAmount);
+    });
+
+    it("Should retrieve the money from the sender", async function () {
+      const {contract, title, otherAccount, investAmount, owner} = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      const initialBalance = await otherAccount.provider.getBalance(otherAccount.getAddress());
+      const target = initialBalance - BigInt(investAmount);
+
+      const approve = await contract.connect(otherAccount).approve(contract.getAddress(), investAmount);
+      const approveWait = await approve.wait();
+
+      const gasUsedApprove = (approveWait?.gasUsed ?? 0n) * (approveWait?.gasPrice ?? 0n);
+
+      const investment = await contract.connect(otherAccount).invest(title, {value: investAmount});
+      const investmentWait = await investment.wait();
+
+      const gasUsed = (investmentWait?.gasUsed ?? 0n) * (investmentWait?.gasPrice ?? 0n);
+
+      expect(await otherAccount.provider.getBalance(otherAccount.getAddress())).to.be.eq(target - gasUsed - gasUsedApprove);
+    });
+
+    it("Should send the money to the contract", async function () {
+      const {contract, title, otherAccount, investAmount} = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      const initialBalance = await contract.balanceOf(contract.getAddress());
+
+      await contract.connect(otherAccount).approve(contract.getAddress(), investAmount);
+      await contract.connect(otherAccount).invest(title, {value: investAmount});
+
+      expect(await contract.balanceOf(contract.getAddress())).to.be.eq(initialBalance + BigInt(investAmount));
+    });
+  });
+
+  describe("Get all campaigns", function () {
+    it("Should get all campaigns", async function () {
+      const {contract, title} = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      console.log(await contract.getAllCampaigns());
+
+      // await expect(contract.getCampaigns())
+      //   .to.emit(contract, "NewCampaign")
+      //   .withArgs(title);
     });
   });
 
@@ -183,10 +231,9 @@ describe("HollyFund", function () {
         deployOneYearLockFixture
       );
 
-      await contract.transfer(otherAccount.address, targetAmount);
       await contract.connect(otherAccount).approve(contract.getAddress(), targetAmount);
 
-      await contract.connect(otherAccount).invest(title, { value: targetAmount });
+      await contract.connect(otherAccount).invest(title, {value: targetAmount});
 
       await expect(contract.connect(otherAccount).claim(title))
         .to.be.revertedWith("Only producer can claim");
@@ -197,12 +244,45 @@ describe("HollyFund", function () {
         deployOneYearLockFixture
       );
 
-      await contract.transfer(otherAccount.address, targetAmount);
       await contract.connect(otherAccount).approve(contract.getAddress(), targetAmount);
 
-      await contract.connect(otherAccount).invest(title, { value: targetAmount });
+      await contract.connect(otherAccount).invest(title, {value: targetAmount});
 
       await expect(contract.claim(title)).not.to.be.reverted;
+    });
+
+    it("Should transfert the money to the owner", async function () {
+      const {contract, title, otherAccount, targetAmount, owner} = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      const initialBalance = await owner.provider.getBalance(owner.getAddress());
+      const target = initialBalance + targetAmount;
+
+      await contract.connect(otherAccount).approve(contract.getAddress(), targetAmount);
+
+      await contract.connect(otherAccount).invest(title, {value: targetAmount});
+      const claimed = await contract.claim(title);
+      const claimedWait = await claimed.wait();
+
+      const gasUsed = (claimedWait?.gasUsed ?? 0n) * (claimedWait?.gasPrice ?? 0n);
+
+      expect(await owner.provider.getBalance(owner.getAddress())).to.be.eq(target - gasUsed);
+    });
+
+    it("Should retrieve the money from the contract", async function () {
+      const {contract, title, otherAccount, targetAmount} = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      const initialBalance = await contract.balanceOf(contract.getAddress());
+
+      await contract.connect(otherAccount).approve(contract.getAddress(), targetAmount);
+      await contract.connect(otherAccount).invest(title, {value: targetAmount});
+
+      await contract.claim(title);
+
+      expect(await contract.balanceOf(contract.getAddress())).to.be.eq(initialBalance);
     });
   });
 });
