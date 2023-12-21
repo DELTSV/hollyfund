c2cf6f274b717cb9fa608fdb724ab33728265812
Enzo Soares <enzoayrsoares@gmail.com>
2023-12-21 20:05:34 +0100

    Styling campaign cards

--- workspaces/frontend/src/features/campaign/card/CampaignCard.tsx
+++ workspaces/frontend/src/features/campaign/card/CampaignCard.tsx
@@ -1,6 +1,7 @@
 import {Campaign} from "../";
 import {useMemo} from "react";
-import {card} from "./style.module.css";
+import {card, bar, progress} from "./style.module.css";
+import {gradientText, gradientBar} from "../../../utils";
 
 type CampaignCardProps = {
   campaign: Campaign
@@ -8,18 +9,21 @@ type CampaignCardProps = {
 }
 
 export const CampaignCard = ({campaign, onClick}: CampaignCardProps) => {
-  const [funds, percentage] = useMemo(() => {
+  const [funds, target, percentage] = useMemo(() => {
     const funds = Number(campaign.totalAmount), target = Number(campaign.targetAmount);
-    return [funds, funds/target*100]
+    return [funds, target, funds/target*100]
   }, [campaign])
   const handleClick = () => onClick(campaign.title);
 
-  return <li className={card}>
-    <button onClick={handleClick}>
+  return <li className={card} onClick={handleClick}>
+    <button>
       <h3>{campaign.title}</h3>
       <footer>
-        <h4>{funds} ETH raised</h4>
-        <h5>{percentage}% funded</h5>
+        <h4><span className={gradientText}>{funds}</span> / {target} ETH raised</h4>
+        <div id={bar}>
+          <div id={progress} className={gradientBar} style={{width: `${percentage}%`}}></div>
+        </div>
+        <h5 className={gradientText}>{percentage}% funded</h5>
       </footer>
     </button>
   </li>;
--- workspaces/frontend/src/features/campaign/list/CampaignList.tsx
+++ workspaces/frontend/src/features/campaign/list/CampaignList.tsx
@@ -2,6 +2,7 @@ import {useContext} from "react";
 import {CampaignContext} from "../context";
 import {CampaignCard} from "../";
 import {title, list} from "./style.module.css";
+import {gradientText} from "../../../utils"
 
 export const CampaignList = () => {
   const {campaigns} = useContext(CampaignContext);
@@ -10,13 +11,13 @@ export const CampaignList = () => {
   else if (campaigns === null) return <h2 className={title}>We have trouble loading campaign list</h2>;
   else if (campaigns.length === 0) return <h2 className={title}>We have trouble loading campaign list</h2>;
   return <>
-    <h2 className={title}>Explore <span>{campaigns.length} projects</span></h2>
+    <h2 className={title}>Explore <span className={gradientText}>{campaigns.length} projects</span></h2>
     <ul className={list}>
       {campaigns.map(campaign =>
         <CampaignCard
           key={campaign.title}
           campaign={campaign}
-          onClick={_ => {}}
+          onClick={_ => console.log(_)}
         />
       )}
     </ul>
