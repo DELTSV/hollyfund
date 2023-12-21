c86c35d0e7d78a4f3d5d8ed11102babd217cd858
Enzo Soares <enzoayrsoares@gmail.com>
2023-12-21 23:22:08 +0100

    Styling modal

--- workspaces/frontend/src/features/campaign/details/CampaignDetails.tsx
+++ workspaces/frontend/src/features/campaign/details/CampaignDetails.tsx
@@ -1,9 +1,32 @@
 import { useContext } from "react";
 import { CampaignContext } from "..";
+import { details } from "./style.module.css";
 
 export const CampaignDetails = () => {
-  const {selectedCampaign} = useContext(CampaignContext);
+  const {selectedCampaign, unselectCampaign} = useContext(CampaignContext);
 
   if (!selectedCampaign) return null;
-  return <dialog open>{selectedCampaign.campaignTitle}</dialog>;
+  return <dialog className={details} onClick={unselectCampaign} open>
+    <article onClick={e => e.stopPropagation()}>
+      <header>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+      </header>
+      <section>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+        <h1>{selectedCampaign.campaignTitle}</h1>
+      </section>
+    </article>
+  </dialog>;
 }
